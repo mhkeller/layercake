5eaf84f53c518a521bf0a76f58336248f4228bce
mhkeller <code@mhkeller.com>
2019-12-16 23:51:25 -0500

    reorganize

--- LayerCake/index.js
+++ LayerCake/index.js
@@ -1,3 +1,4 @@
 export { default as LayerCake } from './LayerCake.svelte';
-export { default as Html } from './Html.svelte';
-export { default as Svg } from './Svg.svelte';
+export { default as Html } from './Layouts/Html.svelte';
+export { default as Svg } from './Layouts/Svg.svelte';
+export { default as Canvas } from './Layouts/Canvas.svelte';

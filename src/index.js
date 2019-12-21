cc5ee233547a2e0bf611535733a6d34b86cc75b4
mhkeller <code@mhkeller.com>
2019-12-21 15:08:52 -0500

    add lib files

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ export { default as LayerCake } from './LayerCake.svelte';
 export { default as Html } from './Layouts/Html.svelte';
 export { default as Svg } from './Layouts/Svg.svelte';
 export { default as Canvas } from './Layouts/Canvas.svelte';
+
+export { default as scaleCanvas } from './lib/scaleCanvas.js';
+export { default as flatten } from './lib/flatten.js';
+export { default as uniques } from './lib/uniques.js';
+export { default as calcExtents } from './lib/calcExtents.js';
---
+++ src/lib/uniques.js
@@ -0,0 +1,29 @@
+/* --------------------------------------------
+ *
+ * Calculate uniqe values from a list with an optional iterator string or function
+ * By default return the transformed value if iteratee exists
+ *
+ * --------------------------------------------
+ */
+export default function uniques (list, iteratee, transform = true) {
+	if (!Array.isArray(list)) {
+		console.error('LayerCake error: Input value to `uniques` must be a list.');
+		return null;
+	};
+	const ll = list.length;
+	const iterater = typeof iteratee === 'function';
+	const key = typeof iteratee !== 'undefined';
+	const seen = [];
+	const result = [];
+	for (let i = 0; i < ll; i++) {
+		const d = list[i];
+		const computed = iterater ? iteratee(d) : key === true ? d[iteratee] : d;
+		if (!seen.includes(computed)) {
+			seen.push(computed);
+			if (transform === false) {
+				result.push(d);
+			}
+		}
+	}
+	return transform === false ? result : seen;
+}

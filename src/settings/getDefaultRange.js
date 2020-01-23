ca995761b70c48531040a1395b1c3088f64b05d1
mhkeller <code@mhkeller.com>
2020-01-23 13:16:00 -0500

    update range calculation defaults

--- src/settings/getDefaultRange.js
+++ src/settings/getDefaultRange.js
@@ -1,31 +1,18 @@
 /* eslint-disable no-nested-ternary */
-export default {
-	x: (width, height, reverse, range) => {
-		return reverse
-			? [width, 0]
-			: typeof range === 'function'
-				? range({ width, height })
-				: (range || [0, width]);
-	},
-	y: (width, height, reverse, range) => {
-		return reverse
-			? [height, 0]
-			: typeof range === 'function'
-				? range({ width, height })
-				: (range || [0, height]);
-	},
-	z: (width, height, reverse, range) => {
-		return reverse
-			? [height, 0]
-			: typeof range === 'function'
-				? range({ width, height })
-				: (range || [0, height]);
-	},
-	r: (width, height, reverse, range) => {
-		return !range
-			? reverse ? [25, 1] : [1, 25]
-			: typeof range === 'function'
-				? range({ width, height })
-				: range;
-	}
-};
+function calcBaseRange(s, width, height, reverse) {
+	const config = {
+		x: reverse === true ? [width, 0] : [0, width],
+		y: reverse === true ? [height, 0] : [0, height],
+		z: reverse === true ? [width, 0] : [0, width],
+		r: reverse === true ? [25, 1] : [1, 25],
+	};
+	return config[s];
+}
+
+export default function getDefaultRange(s, width, height, reverse, range) {
+	return !range
+		? calcBaseRange(s, width, height, reverse)
+		: typeof range === 'function'
+			? range({ width, height })
+			: range;
+}

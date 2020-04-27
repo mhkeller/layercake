919f98221087fbe42557db7f857ee28dcab1b424
mhkeller <code@mhkeller.com>
2020-04-27 01:17:34 -0400

    add suffix

--- src/lib/style.js
+++ src/lib/style.js
@@ -1,9 +1,9 @@
-export default function style(obj) {
+export default function style(obj, suffix = '') {
 	const keys = Object.keys(obj);
 	const l = keys.length;
 	let s = '';
 	for (let i = 0; i < l; i += 1) {
-		s += `${keys[i]}:${obj[keys[i]]};`;
+		s += `${keys[i]}:${obj[keys[i]]}${suffix};`;
 	}
 	return s;
 }

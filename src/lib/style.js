d7e3273715ca85872ec71932846541fd27359faf
mhkeller <code@mhkeller.com>
2020-04-30 21:08:12 -0400

    remove helper

--- src/lib/style.js
+++
@@ -1,9 +0,0 @@
-export default function style(obj, suffix = '') {
-	const keys = Object.keys(obj);
-	const l = keys.length;
-	let s = '';
-	for (let i = 0; i < l; i += 1) {
-		s += `${keys[i]}:${obj[keys[i]]}${suffix};`;
-	}
-	return s;
-}

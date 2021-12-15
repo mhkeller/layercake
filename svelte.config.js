94ed56b1c25e3478c984ad9ea029877ba7e020d8
mhkeller <code@mhkeller.com>
2021-12-15 13:12:36 -0500

    update exports

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,12 @@ import { normalizePath } from 'vite';
 import path from 'path';
 import adapter from '@sveltejs/adapter-auto';
 
+const internalDirs = [
+	'helpers',
+	'settings',
+	'utils'
+]
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	root: normalizePath(path.resolve('./')),
@@ -9,7 +15,15 @@ const config = {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		target: '#svelte',
+		package: {
+			exports: (filepath) => {
+				if (filepath.endsWith('.d.ts')) return false;
+				return !filepath.includes('utils')
+					&& !filepath.includes('helpers')
+					&& !filepath.includes('settings')
+			}
+		}
 	}
 };
 

f8dd2d38a1a8868e70a00182a6898a98a60d28a6
mhkeller <code@mhkeller.com>
2021-12-19 14:03:14 -0500

    fix endpoint issue and build

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,6 @@
 <script context="module">
 	export async function load({ fetch }) {
-		const res = await fetch(`./api/guide-sections.json`);
+		const res = await fetch(`/api/guide-sections.json`);
 		const data = await res.json();
 
 		if (res.status === 200) {

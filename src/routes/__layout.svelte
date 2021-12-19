9d01b71a438980a44bbbffe0b166e3736ee05350
mhkeller <code@mhkeller.com>
2021-12-19 13:09:00 -0500

    add layout fetch

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,22 +1,23 @@
 <script context="module">
-	// export async function load({ fetch }) {
-	// 	const res = await fetch(`./api/guide-sections.json`);
-	// 	const data = await res.json();
+	export async function load({ fetch }) {
+		const res = await fetch(`./api/guide-sections.json`);
+		const data = await res.json();
 
-	// 	if (res.status === 200) {
-	// 		return {
-	// 			props: {
-	// 				sections: data
-	// 			}
-	// 		};
-	// 	} else {
-	// 		return {
-	// 			status: res.status,
-	// 			error: new Error(`Could not load guide sections: ${data.message}`)
-	// 		}
-	// 	}
-	// }
+		if (res.status === 200) {
+			return {
+				props: {
+					sections: data
+				}
+			};
+		} else {
+			return {
+				status: res.status,
+				error: new Error(`Could not load guide sections: ${data.message}`)
+			}
+		}
+	}
 </script>
+
 <script lang="ts">
 	import '../app.css';
 	import '../hljs.css';

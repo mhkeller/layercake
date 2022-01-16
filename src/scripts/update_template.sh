236a551b5dd9fbe81c2a3345ce76827194c34315
mhkeller <code@mhkeller.com>
2022-01-16 16:12:13 -0500

    auto-deploy

--- src/scripts/update_template.sh
+++ src/scripts/update_template.sh
@@ -1,6 +1,6 @@
 # fetch svelte-app
 rm -rf src/scripts/svelte-app
-node_modules/.bin/degit mhkeller/layercake-template#kit src/scripts/svelte-app
+node_modules/.bin/degit mhkeller/layercake-template#main src/scripts/svelte-app
 
 # remove App.svelte (will be recreated client-side) and node_modules
 rm -rf src/scripts/svelte-app/src/routes/index.svelte

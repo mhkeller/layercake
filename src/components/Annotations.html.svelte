62f5c46f96d80cf89b5b5abc7dafee1b9122b902
mhkeller <code@mhkeller.com>
2022-01-19 15:45:42 -0500

    set required props

--- src/components/Annotations.html.svelte
+++ src/components/Annotations.html.svelte
@@ -5,7 +5,7 @@
  <script>
 	 const vals = ['top', 'right', 'bottom', 'left'];
 
-	 /** @type {Array} [annotations=[]] – A list of annotation objects. It expects values of `top`, `right`, `bottom` and `left` whose values are CSS values like `'10px'` or `'5%'` that will be used to absolutely position the text div. See the [Column](https://layercake.graphics/example/Column) chart example for the schema and options. */
+	 /** @type {Array} annotations=[] – A list of annotation objects. It expects values of `top`, `right`, `bottom` and `left` whose values are CSS values like `'10px'` or `'5%'` that will be used to absolutely position the text div. See the [Column](https://layercake.graphics/example/Column) chart example for the schema and options. */
 	 export let annotations = [];
 
 	 /** @type {Function} [getText=d => d.text] – An accessor function to get the field to display. */

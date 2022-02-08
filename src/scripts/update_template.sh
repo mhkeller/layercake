1e465029918268067311d2b69d8a0be6ca06785b
mhkeller <code@mhkeller.com>
2022-02-07 23:35:31 -0500

    update build

--- src/scripts/update_template.sh
+++ src/scripts/update_template.sh
@@ -5,10 +5,10 @@ node_modules/.bin/degit mhkeller/layercake-template#main src/scripts/svelte-app
 # remove App.svelte (will be recreated client-side) and node_modules
 rm -rf src/scripts/svelte-app/src/routes/index.svelte
 rm -rf src/scripts/svelte-app/node_modules
-rm -rf src/scripts/svelte-app/src/routes/data
-mkdir src/scripts/svelte-app/src/routes/data
-rm -rf src/scripts/svelte-app/src/routes/components
-mkdir src/scripts/svelte-app/src/routes/components
+rm -rf src/scripts/svelte-app/src/routes/_data
+# mkdir src/scripts/svelte-app/src/routes/data
+rm -rf src/scripts/svelte-app/src/routes/_components
+# mkdir src/scripts/svelte-app/src/routes/components
 
 # build svelte-app.json
 node src/scripts/build-svelte-app-json.js `find src/scripts/svelte-app -type f`

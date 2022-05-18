24e27f55b37d7433c97fdf2c5eda3acc1d705c82
Chris Ryan <creditdesign@gmail.com>
2022-05-18 11:35:11 +0100

    Adds map example using pre-projected data ref #61
    - Adds MapSvgPreProjected components in 'examples' and '_examples_ssr' folders.
    - Adds links to editable versions on the svelte repl.
    - Adds pre-projected toposjon file.

---
+++ src/content/examples/MapSvgPreProjected.md
@@ -0,0 +1,3 @@
+A map component using pre-projected topojson. Adapted from [d3-selection](https://github.com/d3/d3-selection), the SVG map component uses Layer Cake's built-in `raise` function to ensure the hovered-over SVG element is always on top of its siblings. The tooltip uses Svelte's [createEventDispatcher](https://svelte.dev/docs#createEventDispatcher).
+
+The topojson file `states-albers-10m.json` is taken from the [U.S. Atlas TopoJSON](https://github.com/topojson/us-atlas) repository. This file is designed to fit a 975×610 viewport. Within the map component we can pass in [d3.geoIdentity](https://github.com/d3/d3-geo/blob/main/README.md#geoIdentity) in place of a projection function, in order to scale the SVG to fit its container.
---
+++ src/routes/_examples/MapSvgPreProjected.svelte
@@ -0,0 +1,91 @@
+<script>
+	import { LayerCake, Svg, Html } from 'layercake';
+	import { feature } from 'topojson-client';
+	import { geoIdentity } from 'd3-geo';
+	import { scaleQuantize } from 'd3-scale';
+	import { format } from 'd3-format';
+
+	import MapSvg from '../../_components/Map.svg.svelte';
+	import Tooltip from '../../_components/Tooltip.html.svelte';
+
+	// This example loads json data as json using @rollup/plugin-json
+	import usStates from '../../_data/states-albers-10m.json';
+	import stateData from '../../_data/us-states-data.json';
+
+	const colorKey = 'myValue';
+
+	/* --------------------------------------------
+	 * Create lookups to more easily join our data
+	 * `dataJoinKey` is the name of the field in the data
+	 * `mapJoinKey` is the name of the field in the map file
+	 */
+	const dataJoinKey = 'name';
+	const mapJoinKey = 'name';
+	const dataLookup = new Map();
+
+	const geojson = feature(usStates, usStates.objects.states);
+	const projection = geoIdentity;
+
+	stateData.forEach(d => {
+		dataLookup.set(d[dataJoinKey], d);
+	});
+
+	let evt;
+	let hideTooltip = true;
+
+	// Create a flat array of objects that LayerCake can use to measure
+	// extents for the color scale
+	const flatData = geojson.features.map(d => d.properties);
+	const colors = ['#ffdecc', '#ffc09c', '#ffa06b', '#ff7a33'];
+
+	const addCommas = format(',');
+</script>
+
+<style>
+	/*
+		The wrapper div needs to have an explicit width and height in CSS.
+		It can also be a flexbox child or CSS grid element.
+		The point being it needs dimensions since the <LayerCake> element will
+		expand to fill it.
+	*/
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
+
+<div class="chart-container">
+	<LayerCake
+		data={geojson}
+		z={d => dataLookup.get(d[mapJoinKey])[colorKey]}
+		zScale={scaleQuantize()}
+		zRange={colors}
+		{flatData}
+	>
+		<Svg>
+			<MapSvg
+				{projection}
+				on:mousemove={event => evt = hideTooltip = event}
+				on:mouseout={() => hideTooltip = true}
+			/>
+		</Svg>
+
+		<Html
+			pointerEvents={false}
+		>
+			{#if hideTooltip !== true}
+				<Tooltip
+					{evt}
+					let:detail
+				>
+					<!-- For the tooltip, do another data join because the hover event only has the data from the geography data -->
+					{@const tooltipData = { ...detail.props, ...dataLookup.get(detail.props[mapJoinKey]) }}
+					{#each Object.entries(tooltipData) as [key, value]}
+						{@const keyCapitalized = key.replace(/^\w/, d => d.toUpperCase())}
+						<div class="row"><span>{keyCapitalized}:</span> {typeof value === 'number' ? addCommas(value) : value}</div>
+					{/each}
+				</Tooltip>
+			{/if}
+		</Html>
+	</LayerCake>
+</div>

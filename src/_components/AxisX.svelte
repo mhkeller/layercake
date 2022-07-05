3f9c4665bfd9a2ae8ba1c494d3916df4e3580a32
Sean Lynch <techniq35@gmail.com>
2022-07-05 16:13:19 -0400

    Simplify AxisX by using Math.max() instead of d3-array max()

--- src/_components/AxisX.svelte
+++ src/_components/AxisX.svelte
@@ -4,7 +4,6 @@
  -->
 <script>
 	import { getContext } from 'svelte';
-	import { max } from 'd3-array';
 	const { width, height, xScale, yRange } = getContext('LayerCake');
 
 	/** @type {Boolean} [gridlines=true] - Extend lines from the ticks into the chart space */
@@ -20,7 +19,7 @@
 	export let snapTicks = false;
 
 	/** @type {Function} [formatTick=d => d] - A function that passes the current tick value and expects a nicely formatted value in return. */
-	export let formatTick = d => d;
+	export let formatTick = (d) => d;
 
 	/** @type {Number|Array|Function} [ticks] - If this is a number, it passes that along to the [d3Scale.ticks](https://github.com/d3/d3-scale) function. If this is an array, hardcodes the ticks to those values. If it's a function, passes along the default tick values and expects an array of tick values in return. If nothing, it uses the default ticks supplied by the D3 function. */
 	export let ticks = undefined;
@@ -33,12 +32,13 @@
 
 	$: isBandwidth = typeof $xScale.bandwidth === 'function';
 
-	$: tickVals = Array.isArray(ticks) ? ticks :
-		isBandwidth ?
-			$xScale.domain() :
-			typeof ticks === 'function' ?
-				ticks($xScale.ticks()) :
-					$xScale.ticks(ticks);
+	$: tickVals = Array.isArray(ticks)
+		? ticks
+		: isBandwidth
+		? $xScale.domain()
+		: typeof ticks === 'function'
+		? ticks($xScale.ticks())
+		: $xScale.ticks(ticks);
 
 	function textAnchor(i) {
 		if (snapTicks === true) {
@@ -53,31 +53,38 @@
 	}
 </script>
 
-<g class='axis x-axis' class:snapTicks>
+<g class="axis x-axis" class:snapTicks>
 	{#each tickVals as tick, i}
-		<g class='tick tick-{ i }' transform='translate({$xScale(tick)},{max($yRange)})'>
+		<g class="tick tick-{i}" transform="translate({$xScale(tick)},{Math.max(...$yRange)})">
 			{#if gridlines !== false}
-				<line class="gridline" y1='{$height * -1}' y2='0' x1='0' x2='0'></line>
+				<line class="gridline" y1={$height * -1} y2="0" x1="0" x2="0" />
 			{/if}
 			{#if tickMarks === true}
-				<line class="tick-mark" y1='{0}' y2='{6}' x1='{xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}' x2='{xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}'></line>
+				<line
+					class="tick-mark"
+					y1={0}
+					y2={6}
+					x1={xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}
+					x2={xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}
+				/>
 			{/if}
 			<text
-				x="{xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}"
-				y='{yTick}'
-				dx=''
-				dy=''
-				text-anchor='{textAnchor(i)}'>{formatTick(tick)}</text>
+				x={xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}
+				y={yTick}
+				dx=""
+				dy=""
+				text-anchor={textAnchor(i)}>{formatTick(tick)}</text
+			>
 		</g>
 	{/each}
 	{#if baseline === true}
-		<line class="baseline" y1='{$height + 0.5}' y2='{$height + 0.5}' x1='0' x2='{$width}'></line>
+		<line class="baseline" y1={$height + 0.5} y2={$height + 0.5} x1="0" x2={$width} />
 	{/if}
 </g>
 
 <style>
 	.tick {
-		font-size: .725em;
+		font-size: 0.725em;
 		font-weight: 200;
 	}
 

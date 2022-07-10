74dde920b99b58375403731cd966ba8596fc0a56
mhkeller <code@mhkeller.com>
2022-07-10 11:56:21 -0400

    dont change flatten fn

--- src/lib/lib/flatten.js
+++ src/lib/lib/flatten.js
@@ -8,16 +8,6 @@ export default function flatten (list) {
 		let flat = [];
 		const l = list.length;
 		for (let i = 0; i < l; i += 1) {
-			const ll = list[i].length;
-			/* --------------------------------------------
-			 * Maybe add any data keys onto the flattened object
-			 * for help with computing z scales on stacked data structures
-			 */
-			for (let j = 0; j < ll; j += 1) {
-				Object.assign(list[i][j], Object.fromEntries(Object.keys(list[i]).filter(k => {
-					return Number.isNaN(+k);
-				}).map(k => [k, list[i][k]])));
-			}
 			flat = flat.concat(list[i]);
 		}
 		return flat;

c9a7b995e4853da00758b5ee699f3b32bbcd36da
mhkeller <code@mhkeller.com>
2023-01-24 14:35:31 -0500

    auto-deploy

--- docs/_app/immutable/modules/pages/example/_slug_/_page.js-4898befc.js
+++ docs/_app/immutable/modules/pages/example/_slug_/_page.js-4898befc.js
@@ -1,1 +1,1 @@
-import"../../../../chunks/index-1885ff30.js";import{l}from"../../../../chunks/_page-6385a99b.js";export{l as load};
+import"../../../../chunks/index-1885ff30.js";import{l}from"../../../../chunks/_page-e43f5175.js";export{l as load};

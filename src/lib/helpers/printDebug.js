99e82b70b28f8afd11633a9f0f84f33c4f3b500b
mhkeller <code@mhkeller.com>
2023-02-11 16:18:10 -0500

    add contrasting color

--- src/lib/helpers/printDebug.js
+++ src/lib/helpers/printDebug.js
@@ -1,5 +1,6 @@
 import findScaleName from './findScaleName.js';
 import isCSSColor from 'is-css-color';
+import Color from 'color';
 
 /* --------------------------------------------
  *
@@ -26,34 +27,61 @@ function printObject(obj) {
 function printScale(s, scale) {
 	const scaleName = findScaleName(scale);
 	console.log(`\t${s}:`, scaleName);
-	const d = colorizeArray(scale.domain())
-	if (d.color === true) {
-		console.log(`\t\tDomain: %cArray%c(${scale.domain().length}) ` + d.vals[0], 'color: #1377e4', 'color: #737373', ...d.vals[1], ']');
+	printValues(scale, 'domain');
+	printValues(scale, 'range');
+}
+
+function printValues(scale, method) {
+	const values = scale[method]();
+	const colorValues = colorizeArray(values);
+	if (colorValues) {
+		printColorArray(colorValues, method, values);
 	} else {
-		console.log(`\t\tDomain:`, scale.domain());
+		console.log(`\t\t${t(method)}:`, values);
 	}
-	const r = colorizeArray(scale.range())
-	if (r.color === true) {
-		console.log(`\t\tRange:    %cArray%c(${scale.range().length}) ` + r.vals[0] + '%c ]', 'color: #1377e4', 'color: #737373', 'color: #1478e4', ...r.vals[1], 'color: #1478e4');
-	} else {
-		console.log(`\t\tRange:`, scale.range());
-	}
+}
+
+function printColorArray(colorValues, method, values) {
+	console.log(
+		`\t\t${t(method)}:    %cArray%c(${values.length}) ` + colorValues[0] + '%c ]',
+		'color: #1377e4',
+		'color: #737373',
+		'color: #1478e4',
+		...colorValues[1],
+		'color: #1478e4'
+	);
+}
+
+/**
+ * To Titlecase
+ */
+function t(str) {
+	return str.replace(/^\w/, d => d.toUpperCase())
 }
 
 function colorizeArray(arr) {
 	const colors = [];
-	const a = arr.map(d => {
+	const a = arr.map((d, i) => {
 		if (isCSSColor(d)) {
 			colors.push(d);
-			return `%c ${d}`;
+			// Add a space to the last item
+			const space = i === arr.length - 1 ? ' ' : '';
+			return `%c ${d}${space}`;
 		}
 		return d;
 	});
 	if (colors.length) {
-		return {
-			color: true,
-			vals: [`%c[ ${a.join(', ')}`, colors.map(d => `background-color: ${d}`)]
-		};
+		// TODO, set contrast color
+		return [
+			`%c[ ${a.join(', ')}`,
+			colors.map(
+				d => `background-color: ${d}; color:${contrast(d)};`
+			)
+		];
 	}
-	return a;
+	return null;
+}
+
+function contrast(color) {
+	return Color(color).isLight() ? '#000' : '#fff';
 }

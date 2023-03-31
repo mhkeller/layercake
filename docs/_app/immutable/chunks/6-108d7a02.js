9b842bc9b2be3c94e2256979f71fe68a2b6472c7
mhkeller <code@mhkeller.com>
2023-03-30 23:52:59 -0400

    auto-deploy

--- docs/_app/immutable/chunks/6-108d7a02.js
+++ docs/_app/immutable/chunks/6-108d7a02.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page-be982cf3.js";import{default as t}from"../components/pages/example-ssr/_slug_/_page.svelte-918359b9.js";export{t as component,r as universal};
+import{_ as r}from"./_page-0142e5a3.js";import{default as t}from"../components/pages/example-ssr/_slug_/_page.svelte-918359b9.js";export{t as component,r as universal};
--- docs/_app/immutable/modules/pages/example-ssr/_slug_/_page.js-17d49607.js
+++ docs/_app/immutable/modules/pages/example-ssr/_slug_/_page.js-17d49607.js
@@ -1,1 +1,1 @@
-import"../../../../chunks/index-4d8f42c1.js";import{l}from"../../../../chunks/_page-be982cf3.js";export{l as load};
+import"../../../../chunks/index-4d8f42c1.js";import{l}from"../../../../chunks/_page-0142e5a3.js";export{l as load};

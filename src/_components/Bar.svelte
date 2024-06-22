950dcd595425ef53e86526fa473a822c1a3ee6ef
mhkeller <code@mhkeller.com>
2024-06-22 17:41:36 -0400

    remove unused var

--- src/_components/Bar.svelte
+++ src/_components/Bar.svelte
@@ -5,7 +5,7 @@
 <script>
 	import { getContext } from 'svelte';
 
-	const { data, xGet, yGet, xScale, yScale, xDomain } = getContext('LayerCake');
+	const { data, xGet, yGet, xScale, yScale } = getContext('LayerCake');
 
 	/** @type {String} [fill='#00bbff'] - The shape's fill color. This is technically optional because it comes with a default value but you'll likely want to replace it with your own color. */
 	export let fill = '#00bbff';

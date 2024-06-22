736832eeead1162001a744c7e8da28e3ef6d4527
mhkeller <code@mhkeller.com>
2024-06-22 18:06:25 -0400

    auto-deploy

---
+++ docs/_app/immutable/entry/start.ZUmzy4n0.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bxguu-gh.js";export{t as start};
--- src/scripts/svelte-app/svelte.config.js
+++ src/scripts/svelte-app/svelte.config.js
@@ -1,22 +1,12 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-static';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
-
 	kit: {
-		// base: dev ? '' : '/your-repo-name', // If building for GitHub pages
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false
-		})
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 

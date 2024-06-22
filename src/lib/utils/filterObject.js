ddcba59716e4110c36e5e0da85906a7dfb34e360
mhkeller <code@mhkeller.com>
2024-06-22 15:18:56 -0400

    remove unneeded object.fromentries polyfill

--- src/lib/utils/filterObject.js
+++ src/lib/utils/filterObject.js
@@ -1,26 +1,3 @@
-// From Object.fromEntries polyfill https://github.com/tc39/proposal-object-from-entries/blob/master/polyfill.js#L1
-function fromEntries(iter) {
-	const obj = {};
-
-	for (const pair of iter) {
-		if (Object(pair) !== pair) {
-			throw new TypeError('iterable for fromEntries should yield objects');
-		}
-		// Consistency with Map: contract is that entry has "0" and "1" keys, not
-		// that it is an array or iterable.
-		const { 0: key, 1: val } = pair;
-
-		Object.defineProperty(obj, key, {
-			configurable: true,
-			enumerable: true,
-			writable: true,
-			value: val
-		});
-	}
-
-	return obj;
-}
-
 /**
 	Remove undefined fields from an object
 	@param {object} obj The object to filter
@@ -28,7 +5,7 @@ function fromEntries(iter) {
 	@returns {object}
 */
 export default function filterObject(obj, comparisonObj = {}) {
-	return fromEntries(
+	return Object.fromEntries(
 		Object.entries(obj).filter(([key, value]) => {
 			return value !== undefined && comparisonObj[key] === undefined;
 		})

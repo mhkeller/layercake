a2a49e4164c96520f2818113cf799fcf72f87ec8
Robert Gieseke <rob.g@web.de>
2025-03-17 11:18:40 +0100

    Remove half-working ARIA code and A11Y warning
    Running `svelte-check` reports a missing `aria-valuenow` attribute for the
    range slider (which only has lower and upper value).
    The current code wasn't actually keyboard accessible.

--- src/_components/Brush.html.svelte
+++ src/_components/Brush.html.svelte
@@ -91,16 +91,12 @@
 	$: right = 100 * (1 - max);
 </script>
 
+<!-- svelte-ignore a11y_no_static_element_interactions -->
 <div
 	bind:this={brush}
 	class="brush-outer"
 	on:mousedown|stopPropagation={reset}
 	on:touchstart|stopPropagation={reset}
-	role="slider"
-	aria-valuemin={min}
-	aria-valuemax={max}
-	aria-valuetext="{min} to {max}"
-	tabindex="0"
 >
 	{#if min !== null}
 		<div
@@ -108,33 +104,18 @@
 			on:mousedown|stopPropagation={move}
 			on:touchstart|stopPropagation={move}
 			style="left: {left}%; right: {right}%"
-			role="slider"
-			aria-valuemin={min}
-			aria-valuemax={max}
-			aria-valuetext="{min} to {max}"
-			tabindex="0"
 		></div>
 		<div
 			class="brush-handle"
 			on:mousedown|stopPropagation={adjust_min}
 			on:touchstart|stopPropagation={adjust_min}
 			style="left: {left}%"
-			role="slider"
-			aria-valuemin={min}
-			aria-valuemax={max}
-			aria-valuetext="{min} to {max}"
-			tabindex="0"
 		></div>
 		<div
 			class="brush-handle"
 			on:mousedown|stopPropagation={adjust_max}
 			on:touchstart|stopPropagation={adjust_max}
 			style="right: {right}%"
-			role="slider"
-			aria-valuemin={min}
-			aria-valuemax={max}
-			aria-valuetext="{min} to {max}"
-			tabindex="0"
 		></div>
 	{/if}
 </div>

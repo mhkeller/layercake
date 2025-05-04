fe87773d6592ece2b9a6102b73f8297767620540
Robert Gieseke <rob.g@web.de>
2025-05-04 10:41:24 +0200

    Revert "try firefox for playwright tests"
    This reverts commit b1a61b7eafb5a7a520642613a70105e5b029cba4.

--- playwright.config.js
+++ playwright.config.js
@@ -35,15 +35,15 @@ export default defineConfig({
 
 	/* Configure projects for major browsers */
 	projects: [
-		// {
-		// 	name: 'chromium',
-		// 	use: { ...devices['Desktop Chrome'] }
-		// },
-
 		{
-			name: 'firefox',
-			use: { ...devices['Desktop Firefox'] }
-		},
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] }
+		}
+
+		// {
+		// 	name: 'firefox',
+		// 	use: { ...devices['Desktop Firefox'] }
+		// },
 
 		// {
 		// 	name: 'webkit',

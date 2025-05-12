65e2752acac09bb551fa1c45b6b38f8ecec6fa33
Robert Gieseke <rob.g@web.de>
2025-05-12 09:58:48 +0200

    skip tests for pages without component property table

--- test/e2e/component-properties-table.spec.js
+++ test/e2e/component-properties-table.spec.js
@@ -1,24 +1,17 @@
 import { test, expect } from '@playwright/test';
-
 import { readdirSync } from 'fs';
 
-const componentFilenames = readdirSync('./docs/components')
-	.filter(i => i.endsWith('svelte.html'))
-	.filter(
-		i =>
-			![
-				'AreaStacked.svelte.html',
-				'BarStacked.svelte.html',
-				'ColumnStacked.svelte.html',
-				'GroupLabels.html.svelte.html'
-			].includes(i)
-	); // skipping components without properties
+const componentFilenames = readdirSync('./docs/components').filter(i => i.endsWith('svelte.html'));
 
 componentFilenames.forEach(filename => {
 	const url = `/components/${filename.replace('.svelte.html', '.svelte')}`;
 
 	test(`Property table content for ${url}`, async ({ page }) => {
 		await page.goto(url);
-		await expect(page.locator('div#params-table table')).toMatchAriaSnapshot();
+
+		const locator = page.locator('div#params-table table');
+		test.skip((await locator.count()) === 0, `No properties table in '${url}'`);
+
+		await expect(locator).toMatchAriaSnapshot();
 	});
 });

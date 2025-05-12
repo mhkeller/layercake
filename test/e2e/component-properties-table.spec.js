321c43ec64018fbf6f67740e1cdcfa96d6fca400
Robert Gieseke <rob.g@web.de>
2025-05-12 23:15:56 +0200

    check code for exported properties

--- test/e2e/component-properties-table.spec.js
+++ test/e2e/component-properties-table.spec.js
@@ -1,6 +1,9 @@
 import { test, expect } from '@playwright/test';
 import { readdirSync } from 'fs';
 
+// For each component page it is checked whether the table is matching the stored ARIA snapshot.
+// Compoonent pages with no properties are skipped.
+
 const componentFilenames = readdirSync('./docs/components').filter(i => i.endsWith('svelte.html'));
 
 componentFilenames.forEach(filename => {
@@ -9,9 +12,9 @@ componentFilenames.forEach(filename => {
 	test(`Property table content for ${url}`, async ({ page }) => {
 		await page.goto(url);
 
-		const locator = page.locator('div#params-table table');
-		test.skip((await locator.count()) === 0, `No properties table in '${url}'`);
+		const codeContent = await page.textContent('div.contents pre');
+		test.skip(!codeContent.includes('export let'), `No properties table in '${url}'`);
 
-		await expect(locator).toMatchAriaSnapshot();
+		await expect(page.locator('div#params-table table')).toMatchAriaSnapshot();
 	});
 });

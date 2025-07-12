b84629270162a35a773fb0cb2280a8ae98b790e0
mhkeller <code@mhkeller.com>
2025-07-12 17:49:49 -0400

    toTitleCase.js

--- src/lib/helpers/toTitleCase.js
+++ src/lib/helpers/toTitleCase.js
@@ -1,3 +1,8 @@
+/**
+ * Convert the first character of a string to uppercase.
+ * @param {string} str The string to convert.
+ * @returns {string} The string with the first character in uppercase.
+ */
 export default function toTitleCase(str) {
 	return str.replace(/^\w/, d => d.toUpperCase());
 }

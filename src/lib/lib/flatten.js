73270fb44f67cb884e82f2ed77104758d5dda549
mhkeller <code@mhkeller.com>
2025-07-12 17:33:14 -0400

    update types to flatten.js

--- src/lib/lib/flatten.js
+++ src/lib/lib/flatten.js
@@ -4,12 +4,12 @@
 	@param {string|Function} accessor An optional accessor function to map by. If this is a string, it will be transformed into an accessor for that key.
 	@returns {Array<any>}
 */
-export default function flatten(list, accessor = /** @param {any} d */ (d) => d) {
+export default function flatten(list, accessor = /** @param {any} d */ d => d) {
 	if (!Array.isArray(list)) {
 		return list;
 	}
 
-	const acc = typeof accessor === 'string' ? /** @param {any} d */ (d) => d[accessor] : accessor;
+	const acc = typeof accessor === 'string' ? /** @param {any} d */ d => d[accessor] : accessor;
 
 	const firstResult = acc(list[0]);
 	if (Array.isArray(firstResult)) {

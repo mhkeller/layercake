67d1a0f610c3ba3c65e23ad61b1c32ba513ada80
mhkeller <code@mhkeller.com>
2025-07-13 12:56:01 -0400

    WIP annotationsdata.html

--- src/_components/AnnotationsData.html.svelte
+++ src/_components/AnnotationsData.html.svelte
@@ -8,8 +8,43 @@
 	const { xGet, yGet, percentRange } = getContext('LayerCake');
 
 	/**
+	 * @typedef {Object} ArrowSource
+	 * @property {string} anchor - Anchor position, format: `{left|middle|right}-{top|middle|bottom}`
+	 * @property {number} [dx] - Optional horizontal pixel offset
+	 * @property {number} [dy] - Optional vertical pixel offset
+	 */
+
+	/**
+	 * @typedef {Object} ArrowTarget
+	 * @property {string|number} [x] - X position (can be percentage string like "68%" or data value)
+	 * @property {string|number} [y] - Y position (can be percentage string like "48%" or data value)
+	 * @property {number} [dx] - Optional horizontal pixel offset
+	 * @property {number} [dy] - Optional vertical pixel offset
+	 */
+
+	/**
+	 * @typedef {Object} Arrow
+	 * @property {boolean} [clockwise=true] - Direction of arrow curve
+	 * @property {ArrowSource} source - Arrow starting point configuration
+	 * @property {ArrowTarget} target - Arrow ending point configuration
+	 */
+
+	/**
+	 * @typedef {Object} Annotation
+	 * @property {string} text - The text content of the annotation
+	 * @property {number} [dx] - Optional horizontal pixel offset
+	 * @property {number} [dy] - Optional vertical pixel offset
+	 * @property {number} [top] - CSS top position in pixels
+	 * @property {number} [right] - CSS right position in pixels
+	 * @property {number} [bottom] - CSS bottom position in pixels
+	 * @property {number} [left] - CSS left position in pixels
+	 * @property {Array<Arrow>} [arrows] - Optional array of arrow configurations
+	 * @description Additional dynamic properties can be added using data keys (e.g., [xKey]: value, [yKey]: value) for positioning based on chart data dimensions
+	 */
+
+	/**
 	 * @typedef {Object} Props
-	 * @property {Array} annotations - A list of annotation objects.
+	 * @property {Array<Annotation>} annotations - A list of annotation objects.
 	 * @property {Function} [getText] - An accessor function to get the field to display.
 	 * @property {boolean} [pr] - If `true` will set the `top` and `left` CSS positions to percentages instead of pixels.
 	 */

e7c19e32d86f9479063ce6aae5e2735aeaea36ae
mhkeller <code@mhkeller.com>
2025-07-13 20:50:33 -0400

    ignore prerender 500 errors for now

--- svelte.config.js
+++ svelte.config.js
@@ -21,6 +21,10 @@ const config = {
 			'$lib/*': './src/lib/*',
 			layercake: './src/lib/index.js',
 			'layercake/*': './src/lib/*'
+		},
+
+		prerender: {
+			handleHttpError: 'warn'
 		}
 
 		// 	optimizeDeps: {

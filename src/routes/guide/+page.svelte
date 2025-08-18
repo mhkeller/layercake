3b798c224271259cc45e6848df3de51d8ba44d02
Robert Gieseke <rob.g@web.de>
2025-08-18 11:13:24 +0200

    Fix other getBoundingClientRect type and formatting

--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -24,9 +24,12 @@
 	function onresize() {
 		if (container) {
 			const { top } = container.getBoundingClientRect();
-			positions = [].map.call(anchors, /** @param {HTMLAnchorElement} anchor */ anchor => {
-				return anchor.getBoundingClientRect().top - top;
-			});
+			positions = [].map.call(
+				anchors,
+				/** @param {HTMLAnchorElement} anchor */ anchor => {
+					return anchor.getBoundingClientRect().top - top;
+				}
+			);
 		}
 	}
 

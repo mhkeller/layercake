127aa9f71fa9b3b0656138a2a2ff3f2141ac0a1e
Robert Gieseke <rob.g@web.de>
2025-08-18 16:19:17 +0200

    Add CurveFactory type

--- src/_components/Line-D3.svelte
+++ src/_components/Line-D3.svelte
@@ -8,10 +8,11 @@
 
 	const { data, xGet, yGet } = getContext('LayerCake');
 
+	/** @typedef {import('d3-shape').CurveFactory} CurveFactory */
 	/**
 	 * @typedef {Object} Props
 	 * @property {string} [stroke='#ab00d6'] - The shape's fill color. This is technically optional because it comes with a default value but you'll likely want to replace it with your own color.
-	 * @property {Function} [curve=curveLinear] - An optional D3 interpolation function. See [d3-shape](https://github.com/d3/d3-shape#curves) for options. Pass this function in uncalled, i.e. without the open-close parentheses.
+	 * @property {CurveFactory} [curve=curveLinear] - An optional D3 interpolation function. See [d3-shape](https://github.com/d3/d3-shape#curves) for options. Pass this function in uncalled, i.e. without the open-close parentheses.
 	 */
 
 	/** @type {Props} */

3a8a9fc5335b63c800a6a896187aac5c10e04fe3
Robert Gieseke <rob.g@web.de>
2025-09-01 13:39:52 +0200

    Keep function in vite.config.js for re-usability in template

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,38 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import dsv from '@rollup/plugin-dsv';
 import path from 'path';
-import processCsvValue from './src/lib/utils/processCsvValue.js'
+
+/**
+ * Convert numerical values from a CSV file with `plugin-dsv`
+ * @param {string} value
+ * @returns {string|number}
+ */
+const processCsvValue = value => {
+	// Examples:
+	// '' => ''
+	// 'foo' => 'foo'
+	// '2018-07-22T22:25:55Z' => '2018-07-22T22:25:55Z'
+	// '1.23' => 1.23
+	// '42' => 42
+	const number = Number(value);
+	if (value !== '' && !isNaN(number)) {
+		return number;
+	} else {
+		return value;
+	}
+};
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
 		sveltekit(),
 		dsv({
-			processRow: (row) => {
-			  Object.keys(row).forEach((key) => {
-				row[key] = processCsvValue(row[key])
-			  });
+			processRow: row => {
+				Object.keys(row).forEach(key => {
+					row[key] = processCsvValue(row[key]);
+				});
 			}
-		  })
+		})
 	],
 	resolve: {
 		alias: {

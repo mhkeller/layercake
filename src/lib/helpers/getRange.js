a851156bbce502b993fa9c54110f9a9802ca7eee
Robert Gieseke <rob.g@web.de>
2026-07-26 14:37:58 +0200

    Update helper functions

--- src/lib/helpers/getRange.js
+++ src/lib/helpers/getRange.js
@@ -1,7 +1,7 @@
-export default function getRange([$scale]) {
-	if (typeof $scale === 'function') {
-		if (typeof $scale.range === 'function') {
-			return $scale.range();
+export default function getRange(scale) {
+	if (typeof scale === 'function') {
+		if (typeof scale.range === 'function') {
+			return scale.range();
 		}
 		console.error("[LayerCake] Your scale doesn't have a `.range` method?");
 	}
